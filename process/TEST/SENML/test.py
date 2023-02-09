62c0402b44688a1712a21ff80f870b3c6eeb28d1
Matteo Scacchetti <scacchettim00@gmail.com>
2023-02-09 23:36:50 +0100

    Bug fix

--- process/TEST/SENML/test.py
+++ process/TEST/SENML/test.py
@@ -2,7 +2,15 @@ import time
 from utils.SenML.SenML_Pack import SenMLPack
 
 pack = SenMLPack()
-pack.insert_senml_record(1, "TEST", time.time(), "u", 1)
-#print(pack.senml_pack_toString())
+pack.insert_senml_record(bver=1, bn="TEST", bt=time.time(), u="u", v=1)
+pack.insert_senml_record(bver=1, bn="TEST", bt=time.time(), u="u", v=1)
+
+for record in pack.get_senml_pack():
+    print(record.toJSON())
+
+print(pack.senml_pack_toString())
 pack.string_to_senml_pack(pack.senml_pack_toString())
-#print(pack.senml_pack_toString())
+print(pack.senml_pack_toString())
+
+for record in pack.get_senml_pack():
+    print(record.toJSON())
--- utils/SenML/SenML_Pack.py
+++ utils/SenML/SenML_Pack.py
@@ -9,25 +9,39 @@ class SenMLPack:
     def __init__(self):
         self._senMLPack = []
 
-    def __int__(self, senMLPack):
-        self._senMLPack = senMLPack
-
     def get_senml_pack(self):
         return self._senMLPack
 
-    def insert_senml_record(self, version=None, name=None, time=None, value=None, unit=None):
+    def set_senml_pack(self, senMLPack):
+        self._senMLPack = senMLPack
+
+    def insert_senml_record(self, bver=None, bn=None, bt=None, bu=None, bv=None, v=None, n=None, u=None, vb=None):
         record = SenMLRecord()
-        if version is not None:
-            record.set_bver(version)
-        if name is not None:
-            record.set_bn(name)
-        if time is not None:
-            record.set_bt(time)
-        if value is not None:
-            record.set_v(value)
-        if unit is not None:
-            record.set_u(unit)
+        if bver is not None:
+            record.set_bver(bver)
+        if bn is not None:
+            record.set_bn(bn)
+        if bt is not None:
+            record.set_bt(bt)
+        if bu is not None:
+            record.set_bu(bu)
+        if bv is not None:
+            record.set_bv(bv)
+        if v is not None:
+            record.set_v(v)
+        if n is not None:
+            record.set_n(n)
+        if u is not None:
+            record.set_u(u)
+        if vb is not None:
+            record.set_vb(vb)
         self._senMLPack.append(record)
+
+    def insert_senml_record_object(self, record):
+        if record is SenMLRecord:
+            self._senMLPack.append(record)
+        else:
+            raise SenmlFormatError("Object format is not support -- required senml") from None
 
     def senml_pack_toString(self):
         return json.dumps(self._senMLPack,
@@ -37,8 +51,15 @@ class SenMLPack:
 
     def string_to_senml_pack(self, payload):
         try:
-            self._senMLPack = json.loads(payload)
-            if len(self._senMLPack) <= 0:
+            records = json.loads(payload)
+            if len(records) > 0:
+                self._senMLPack = []
+
+                for val in records:
+                    record = SenMLRecord()
+                    record.fromJSON(val)
+                    self._senMLPack.append(record)
+            else:
                 logging.warning("Senml pack is empty")
         except Exception as e:
             logging.error(str(e))
